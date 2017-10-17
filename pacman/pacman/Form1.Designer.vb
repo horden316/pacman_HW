c6e67d409dc4af7f09b2f529e2c44310432f5cab
horden316 <horden316@gmail.com>
2017-10-17 21:56:36 +0800

    first commit

---
+++ pacman/pacman/Form1.Designer.vb
@@ -0,0 +1,89 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請不要使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(215, 115)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(55, 56)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = Global.pacman.My.Resources.Resources.ghost
+        Me.PictureBox2.Location = New System.Drawing.Point(404, 226)
+        Me.PictureBox2.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(62, 61)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'Timer2
+        '
+        Me.Timer2.Enabled = True
+        Me.Timer2.Interval = 10
+        '
+        'Timer3
+        '
+        Me.Timer3.Enabled = True
+        Me.Timer3.Interval = 2000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.ClientSize = New System.Drawing.Size(541, 350)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Timer2 As System.Windows.Forms.Timer
+    Friend WithEvents Timer3 As System.Windows.Forms.Timer
+End Class
---
+++ pacman/pacman/Form1.vb
@@ -0,0 +1,131 @@
+﻿Public Class Form1
+    Dim switch As Boolean = True
+    Dim dir As Integer = 0
+    Dim a As Boolean = True
+    Dim pox As Integer = 0
+    Dim poy As Integer = 0
+    Dim way As Integer = 0
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim player_R As Rectangle = New Rectangle(Me.PictureBox1.Location, Me.PictureBox1.Size)
+        Dim ghost_R As Rectangle = New Rectangle(Me.PictureBox2.Location, Me.PictureBox2.Size)
+        If (player_R.IntersectsWith(ghost_R) = True And a = True) Then
+            a = False
+            PictureBox2.Visible = False
+            MessageBox.Show("You Win!")
+            MessageBox.Show("New Game!")
+            Application.Restart()
+        End If
+
+        If dir = 0 Then
+
+            If switch = True Then
+                PictureBox1.Image = My.Resources.pac1
+            Else
+                PictureBox1.Image = My.Resources.pac2
+            End If
+            switch = Not switch
+
+        ElseIf dir = 1 Then
+
+            If switch = True Then
+                PictureBox1.Image = My.Resources.pac11
+            Else
+                PictureBox1.Image = My.Resources.pac22
+            End If
+            switch = Not switch
+
+        ElseIf dir = 2 Then
+
+            If switch = True Then
+                PictureBox1.Image = My.Resources.pac111
+            Else
+                PictureBox1.Image = My.Resources.pac222
+            End If
+            switch = Not switch
+
+
+        ElseIf dir = 3 Then
+
+            If switch = True Then
+                PictureBox1.Image = My.Resources.pac1111
+            Else
+                PictureBox1.Image = My.Resources.pac2222
+            End If
+            switch = Not switch
+        End If
+    End Sub
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        Dim key As Integer = e.KeyValue
+        Me.Text = key
+        If key = 87 Then
+            dir = 2
+            PictureBox1.Top -= 10
+            If PictureBox1.Top < 0 Then
+                PictureBox1.Top = 0
+            End If
+        End If
+        If key = 83 Then
+            dir = 3
+            PictureBox1.Top += 10
+            If PictureBox1.Top > Me.Height - PictureBox1.Height - 42 Then
+                PictureBox1.Top = Me.Height - PictureBox1.Height - 42
+            End If
+        End If
+
+        If key = 65 Then
+            dir = 0
+            PictureBox1.Left -= 10
+            If PictureBox1.Left < 0 Then
+                PictureBox1.Left = 0
+            End If
+        End If
+        If key = 68 Then
+            dir = 1
+            PictureBox1.Left += 10
+            If PictureBox1.Left > Me.Width - PictureBox1.Width - 20 Then
+                PictureBox1.Left = Me.Width - PictureBox1.Width - 20
+            End If
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        If PictureBox2.Left > Me.Width - PictureBox2.Width - 20 Then
+            pox = 0
+        ElseIf PictureBox2.Left < 0 Then
+            pox = 1
+        End If
+        If PictureBox2.Top > Me.Height - PictureBox2.Height - 42 Then
+            poy = 1
+        ElseIf PictureBox2.Top < 0 Then
+            poy = 0
+        End If
+        If way = 0 Then
+            If pox = 1 Then
+                PictureBox2.Left += 1
+            ElseIf pox = 0 Then
+                PictureBox2.Left -= 1
+            End If
+        End If
+        If way = 1 Then
+            If poy = 0 Then
+                PictureBox2.Top += 1
+            ElseIf poy = 1 Then
+                PictureBox2.Top -= 1
+            End If
+        End If
+    End Sub
+
+    Private Sub Timer3_Tick(sender As Object, e As EventArgs) Handles Timer3.Tick
+        Randomize()
+        pox = Int(Rnd() * 2)
+        Randomize()
+        poy = Int(Rnd() * 2)
+        Randomize()
+        way = Int(Rnd() * 2)
+    End Sub
+End Class
